70d461accb7a19be243b8e8ff685ff5e220d6663
Ryan <RyanSimpson94@users.noreply.github.com>
2020-12-11 09:33:30 +0000

    Add files via upload

---
+++ src/components/base/ImgPlaceholder.vue
@@ -0,0 +1,24 @@
+<template>
+  <img :src="placeholderUrl" alt="Placeholder image" />
+</template>
+
+<script>
+export default {
+  name: 'Placeholder-Image',
+  props: {
+    width: {
+      type: Number,
+      default: 350
+    },
+    height: {
+      type: Number,
+      default: 350
+    }
+  },
+  computed: {
+    placeholderUrl() {
+      return `http://via.placeholder.com/${this.width}x${this.height}`;
+    }
+  }
+}
+</script>
