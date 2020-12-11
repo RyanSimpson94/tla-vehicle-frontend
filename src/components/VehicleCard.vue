6b56c754bf1ac7551e9839dec3a7ef5a03a19bfd
Ryan <RyanSimpson94@users.noreply.github.com>
2020-12-11 09:33:50 +0000

    Add files via upload

---
+++ src/components/VehicleCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <section class="vehicle-card display--flex align-items--center flex-direction--column">
+    <h3 class="vehicle-card__title">{{ vehicle.name }}</h3>
+    
+    <img class="vehicle-card__img" :alt="vehicle.name" :src="vehicle.imgUrl" v-if="vehicle.imgUrl" />
+    <app-placeholder-img class="vehicle-card__img" :height="300" :width="450" v-else />
+    
+    <div class="vehicle-card__body">
+      <p v-if="vehicle.description">{{ vehicle.description }}</p>
+
+      <p class="vehicle-card__price text--center" v-if="vehicle.price">Recommended OTR price ranging from
+        <span class="vehicle-card__price__text text--center">{{ vehicle.price }}</span>
+      </p>
+    </div>
+    <slot name="cta"></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'VehicleCard',
+  props: {
+    vehicle: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './../assets/scss/variables.scss';
+
+.vehicle-card {
+  background: white;
+  padding: 0.25em 0.5em 2em 0.5em;
+  margin: 1em;
+  box-shadow: 8px 8px 8px 2px $color-grey--light;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+
+  &__title {
+    margin: 1rem 0;
+    font-size: 200%;
+    font-weight: bold;
+    line-height: 2rem;
+    width: 100%;
+    text-align: center;
+
+    &::after {
+      content: '';
+      display: block;
+      padding-top: 0.5em;
+      width: 100%;
+      border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+    }
+  }
+
+  &__img {
+    min-width: 100%;
+    height: auto;
+    max-width: 350px;
+    vertical-align: middle;
+    margin: 0.5em;
+  }
+
+  &__body {
+    margin: 0.5em;
+  }
+
+  &__price {
+    font-size: 80%;
+    margin-bottom: 1.5em;
+
+    &__text {
+      display: block;
+      font-size: 200%;
+      margin: 0;
+      color: $color-red;
+      font-weight: bolder;
+    }
+  }
+}
+
+</style>
---
+++ src/components/VehicleGroup.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="vehicle-group">
+      <h2 v-if="title">{{ title }}</h2>
+
+      <div class="display--flex flex-direction--row align-items--center flex--wrap">
+        <vehicle-card :vehicle="vehicle" v-for="vehicle in vehicleGroup.vehicles" :key="vehicle.id">
+          <template v-slot:cta>
+            <app-button type="button" size="large" theme="primary" @click="selectVehicle(vehicle)">Select</app-button>
+          </template>
+        </vehicle-card>
+      </div>
+    </section>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+
+export default {
+  name: 'VehicleGroup',
+  components: {
+    'vehicle-card': () => import('./VehicleCard')
+  },
+  props: {
+    title: {
+      type: String,
+      required: false
+    },
+    vehicleGroup: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    ...mapMutations(['selectVehicle'])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .vehicle-group {
+    margin: 1.5em 3em;
+    padding: 1em;
+  }
+</style>
